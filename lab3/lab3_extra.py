cccd88cfa339b6d44be57e65751c91ad9aaaedcf
Trung <tranthetrung9g@gmail.com>
2025-05-07 17:24:45 +0700

    add code for focal loss lab 3

---
+++ lab3/lab3_extra.py
@@ -0,0 +1,67 @@
+import math
+
+def sigmoid(x):
+    return 1/(1 + math.exp(-x))
+
+def loss(yi, y_hati, gamma):
+    p = sigmoid(y_hati)
+    pt = yi * p + (1 - yi) * (1 - p)
+    return -((1 - pt) ** gamma) * math.log(pt)
+
+def f_w0(xi, yi, y_hati, gamma):
+    p = sigmoid(y_hati)
+    pt = yi * p + (1 - yi) * (1 - p)
+    return (gamma * ((1 - pt)**(gamma - 1)) * math.log(pt) - ((1 - pt)**gamma) / pt) * (2 * yi - 1) * p * (1 - p)
+
+def f_w1(xi, yi, y_hati, gamma):
+    p = sigmoid(y_hati)
+    pt = yi * p + (1 - yi) * (1 - p)
+    return ((gamma * ((1 - pt)**(gamma - 1)) * math.log(pt) - ((1 - pt)**gamma) / pt) * (2 * yi - 1) * p * (1 - p)) * xi[0]
+
+def f_w2(xi, yi, y_hati, gamma):
+    p = sigmoid(y_hati)
+    pt = yi * p + (1 - yi) * (1 - p)
+    return ((gamma * ((1 - pt)**(gamma - 1)) * math.log(pt) - ((1 - pt)**gamma) / pt) * (2 * yi - 1) * p * (1 - p)) * xi[1]
+
+def gradient_descent(w0, w1, w2, lr, x, y, gamma = 0, max_step = 1000, tol = 1e-5):
+    N = len(x)
+    y_hat = [(w1 * xi[0] + w2 * xi[1] + w0) for xi in x]
+    f_val = sum(loss(yi, y_hati, gamma) for yi, y_hati in zip(y, y_hat)) / N
+    print(f_val)
+
+    for i in range(max_step):
+        w0_grad = sum(f_w0(xi, yi, y_hati, gamma) for xi, yi, y_hati in zip(x, y, y_hat)) / N
+        w1_grad = sum(f_w1(xi, yi, y_hati, gamma) for xi, yi, y_hati in zip(x, y, y_hat)) / N
+        w2_grad = sum(f_w2(xi, yi, y_hati, gamma) for xi, yi, y_hati in zip(x, y, y_hat)) / N
+
+        w0 -= lr * w0_grad
+        w1 -= lr * w1_grad
+        w2 -= lr * w2_grad
+
+        y_hat = [(w1 * xi[0] + w2 * xi[1] + w0) for xi in x]
+        new_f_val = sum(loss(yi, y_hati, gamma) for yi, y_hati in zip(y, y_hat)) / N
+        print(new_f_val)
+
+        if abs(f_val - new_f_val) < tol:
+            return w0, w1, w2
+
+        f_val = new_f_val
+    return w0, w1, w2
+
+
+if __name__ == "__main__":
+    x = []
+    y = []
+    with open('loan2.csv', 'r') as file:
+        file.readline()
+        for line in file.readlines():
+            xi1, xi2, yi = map(float, line.split(','))
+            x.append([xi1, xi2])
+            y.append(yi)
+
+    w0_init = 0.01
+    w1_init = 0.01
+    w2_init = 0.02
+    lr = 0.1
+
+    w0, w1, w2 = gradient_descent(w0_init, w1_init, w2_init, lr, x, y, gamma=1, max_step=1000)
